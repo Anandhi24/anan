397e9416a8126de31ce1afe6d76de30c1ff88699
Anandhi24 <46163458+Anandhi24@users.noreply.github.com>
2019-06-20 12:23:01 +0530

    Create p2.py

---
+++ p2.py
@@ -0,0 +1,9 @@
+s=input()
+list=[]
+list1=[]
+for i in range(len(s)):
+    if s[i] not in list and s[i].islower():
+        list.append(s[i])
+    else:
+        list1.append(s[i])
+print(len(list))
